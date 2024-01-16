1b7af457e86c01f916eb0247b63b84bfb8ebb149
Astridxx <muratkozan350@gmail.com>
2024-01-16 19:57:40 +0300

    haydn: Add audio-app patch

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,4 +9,11 @@ echo 'Cloning Leica camera'
 git clone --depth=1 https://gitlab.com/Alucard_Storm/haydn-miuicamera.git -b fourteen-leica vendor/xiaomi/haydn-miuicamera
 rm -rf hardware/xiaomi/megvii
 
+# Audio
+echo 'Adding audio-app support'
+cd system/core
+wget https://raw.githubusercontent.com/xiaomi-haydn-devs/Patch-Haydn/14/Audio-app-main/0001-Add-audio-app-support.patch
+patch -p1 <0001-Add-audio-app-support.patch
+cd ../..
+
 echo 'delete vendorsetup.sh from device tree once this is done'
