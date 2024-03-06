717317392e51dc87bfd987086bc6f0b9d6c41c69
Alucard Stormbringer <supersayan1000@gmail.com>
2024-03-06 21:16:38 +0530

    haydn: vendorsetup: Track repos from Horizon org

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,19 +2,19 @@ echo 'Starting to clone stuffs needed to build for Haydn'
 
 # Device common
 echo 'Cloning common device tree'
-git clone https://github.com/xiaomi-haydn-devs/android_device_xiaomi_sm8350-common -b HorizonDroid device/xiaomi/sm8350-common
+git clone https://github.com/HorizonDroid-Devices/android_device_xiaomi_sm8350-common-haydn -b HorizonDroid device/xiaomi/sm8350-common
 
 # Kernel
 echo 'Cloning kernel tree'
-git clone https://github.com/xiaomi-haydn-devs/android_kernel_xiaomi_sm8350.git --recursive --depth=1 -b lineage-21 kernel/xiaomi/sm8350
+git clone https://github.com/HorizonDroid-Devices/android_kernel_xiaomi_sm8350 --recursive --depth=1 -b lineage-21 kernel/xiaomi/sm8350
 
 # Vendor
 echo 'Cloning vendor tree'
-git clone https://github.com/xiaomi-haydn-devs/proprietary_vendor_xiaomi_haydn -b lineage-21 vendor/xiaomi/haydn
+git clone https://github.com/HorizonDroid-Devices/proprietary_vendor_xiaomi_haydn -b lineage-21 vendor/xiaomi/haydn
 
 # Vendor common
 echo 'Cloning common vendor tree'
-git clone https://github.com/xiaomi-haydn-devs/proprietary_vendor_xiaomi_sm8350-common -b lineage-21 vendor/xiaomi/sm8350-common
+git clone https://github.com/HorizonDroid-Devices/proprietary_vendor_xiaomi_sm8350-common-haydn -b lineage-21 vendor/xiaomi/sm8350-common
 
 # Xiaomi
 echo 'Cloning hardware xiaomi'
